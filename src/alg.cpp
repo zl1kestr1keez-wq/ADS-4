97b71b0bc6f5451a2e8052f66574753020fe35be
Denis <zl1kestr1keez@gmail.com>
2026-03-24 10:01:43 +0300

    Сахаров Денис 25-ИСТ-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,6 +14,7 @@ int countPairs2(int *arr, int len, int value) {
   int count = 0;
   int left = 0;
   int right = len - 1;
+  
   while (left < right) {
     int sum = arr[left] + arr[right];
     if (sum == value) {
@@ -21,21 +22,20 @@ int countPairs2(int *arr, int len, int value) {
         int n = right - left + 1;
         count += n * (n - 1) / 2;
         break;
-      } else {
-        int leftCount = 1;
-        int rightCount = 1;
-        while (left + 1 < right && arr[left] == arr[left + 1]) {
-          leftCount++;
-          left++;
-        }
-        while (right - 1 > left && arr[right] == arr[right - 1]) {
-          rightCount++;
-          right--;
-        }
-        count += leftCount * rightCount;
+      }
+      int leftVal = arr[left];
+      int rightVal = arr[right];
+      int leftCount = 0;
+      int rightCount = 0;
+      while (left <= right && arr[left] == leftVal) {
+        leftCount++;
         left++;
+      }
+      while (left <= right && arr[right] == rightVal) {
+        rightCount++;
         right--;
       }
+      count += leftCount * rightCount;
     } else if (sum < value) {
       left++;
     } else {
