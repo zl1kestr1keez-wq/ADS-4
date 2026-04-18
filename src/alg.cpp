d8547bff9baa3d81b3c85f6e4a5ff59019d45395
Denis <zl1kestr1keez@gmail.com>
2026-04-18 23:37:40 +0300

    Сахаров Денис 25-ИСТ-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,25 +1,17 @@
 // Copyright 2021 NNTU-CS
 #include "alg.h"
 int countPairs1(int *arr, int len, int value) {
-  return 0;
-    int count = 0;
-    for (int i = 0; i < len; i++) {
-        for (int j = i + 1; j < len; j++) {
-            if (arr[i] + arr[j] == value) {
-                count++;
-                while (j + 1 < len && arr[j] == arr[j + 1]) {
-                    j++;
-                }
-            }
-        }
-        while (i + 1 < len && arr[i] == arr[i + 1]) {
-            i++;
-        }
+  int count = 0;
+  for (int i = 0; i < len; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        count++;
+      }
     }
-    return count;
+  }
+  return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
     int count = 0;
     for (int i = 0; i < len; i++) {
         int targ = value - arr[i];
@@ -49,7 +41,6 @@ int countPairs2(int *arr, int len, int value) {
     return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
     int count = 0;
     int left = 0;
     int right = len - 1;
